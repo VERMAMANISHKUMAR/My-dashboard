85a6f8134d400827de92b4914d8040e0fceb2335
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2025-05-06 21:02:39 +0530

    add

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+function App() {
+  console.log("React is running!");
+
+  return (
+    <div style={{ backgroundColor: "black", color: "white", padding: "2rem" }}>
+      <h1>React is Running!</h1>
+      <p>Check console log for confirmation!</p>
+      <p>this should implement in admin-dashboard ---    <br/> create a basic admin dashboard using react and extract data from a webpage that contains information about the company, number of sales , revenue last month , revenue last year with the authentication where admin can register as admin and login.</p>
+    </div>
+  );
+}
+
+export default App;
